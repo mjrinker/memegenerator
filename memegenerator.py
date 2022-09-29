1510f77158fb39135fa9773c5e6e19603bf7a655
Matt Rinker <mjrinker@gmail.com>
2022-09-29 14:28:26 -0600

    Reformat code per PEP8

--- memegenerator.py
+++ memegenerator.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 
-import PIL
 from PIL import ImageFont
 from PIL import Image
 from PIL import ImageDraw
@@ -8,103 +7,102 @@ from PIL import ImageDraw
 import sys
 
 
-def make_meme(topString, bottomString, filename):
+def make_meme(top_string, bottom_string, filename):
+    img = Image.open(filename)
+    image_size = img.size
 
-	img = Image.open(filename)
-	imageSize = img.size
+    # find biggest font size that works
+    font_size = int(image_size[1]/5)
+    font = ImageFont.truetype('/Library/Fonts/Impact.ttf', font_size)
+    top_text_size = font.getsize(top_string)
+    bottom_text_size = font.getsize(bottom_string)
+    while top_text_size[0] > image_size[0]-20 or bottom_text_size[0] > image_size[0]-20:
+        font_size = font_size - 1
+        font = ImageFont.truetype('/Library/Fonts/Impact.ttf', font_size)
+        top_text_size = font.getsize(top_string)
+        bottom_text_size = font.getsize(bottom_string)
 
-	# find biggest font size that works
-	fontSize = int(imageSize[1]/5)
-	font = ImageFont.truetype("/Library/Fonts/Impact.ttf", fontSize)
-	topTextSize = font.getsize(topString)
-	bottomTextSize = font.getsize(bottomString)
-	while topTextSize[0] > imageSize[0]-20 or bottomTextSize[0] > imageSize[0]-20:
-		fontSize = fontSize - 1
-		font = ImageFont.truetype("/Library/Fonts/Impact.ttf", fontSize)
-		topTextSize = font.getsize(topString)
-		bottomTextSize = font.getsize(bottomString)
+    # find top centered position for top text
+    top_text_position_x = (image_size[0]/2) - (top_text_size[0]/2)
+    top_text_position_y = 0
+    top_text_position = (top_text_position_x, top_text_position_y)
 
-	# find top centered position for top text
-	topTextPositionX = (imageSize[0]/2) - (topTextSize[0]/2)
-	topTextPositionY = 0
-	topTextPosition = (topTextPositionX, topTextPositionY)
+    # find bottom centered position for bottom text
+    bottom_text_position_x = (image_size[0]/2) - (bottom_text_size[0]/2)
+    bottom_text_position_y = image_size[1] - bottom_text_size[1]
+    bottom_text_position = (bottom_text_position_x, bottom_text_position_y)
 
-	# find bottom centered position for bottom text
-	bottomTextPositionX = (imageSize[0]/2) - (bottomTextSize[0]/2)
-	bottomTextPositionY = imageSize[1] - bottomTextSize[1]
-	bottomTextPosition = (bottomTextPositionX, bottomTextPositionY)
+    draw = ImageDraw.Draw(img)
 
-	draw = ImageDraw.Draw(img)
+    # draw outlines
+    # there may be a better way
+    outline_range = int(font_size/15)
+    for x in range(-outline_range, outline_range+1):
+        for y in range(-outline_range, outline_range+1):
+            draw.text((top_text_position[0]+x, top_text_position[1]+y), top_string, (0,0,0), font=font)
+            draw.text((bottom_text_position[0]+x, bottom_text_position[1]+y), bottom_string, (0,0,0), font=font)
 
-	# draw outlines
-	# there may be a better way
-	outlineRange = int(fontSize/15)
-	for x in range(-outlineRange, outlineRange+1):
-		for y in range(-outlineRange, outlineRange+1):
-			draw.text((topTextPosition[0]+x, topTextPosition[1]+y), topString, (0,0,0), font=font)
-			draw.text((bottomTextPosition[0]+x, bottomTextPosition[1]+y), bottomString, (0,0,0), font=font)
+    draw.text(top_text_position, top_string, (255,255,255), font=font)
+    draw.text(bottom_text_position, bottom_string, (255,255,255), font=font)
 
-	draw.text(topTextPosition, topString, (255,255,255), font=font)
-	draw.text(bottomTextPosition, bottomString, (255,255,255), font=font)
+    img.save('temp.png')
 
-	img.save("temp.png")
 
-def get_upper(somedata):
-	'''
-	Handle Python 2/3 differences in argv encoding
-	'''
-	result = ''
-	try:
-		result = somedata.decode("utf-8").upper()
-	except:
-		result = somedata.upper()
-	return result
+def get_upper(some_data):
+    '''
+    Handle Python 2/3 differences in argv encoding
+    '''
+    result = ''
+    try:
+        result = some_data.decode('utf-8').upper()
+    except:
+        result = some_data.upper()
+    return result
 
-def get_lower(somedata):
-	'''
-	Handle Python 2/3 differences in argv encoding
-	'''
-	result = ''
-	try:
-		result = somedata.decode("utf-8").lower()
-	except:
-		result = somedata.lower()		
 
-	return result
+def get_lower(some_data):
+    '''
+    Handle Python 2/3 differences in argv encoding
+    '''
+    result = ''
+    try:
+        result = some_data.decode('utf-8').lower()
+    except:
+        result = some_data.lower()		
 
+    return result
 
 
 if __name__ == '__main__':
+    args_len = len(sys.argv)
+    top_string = ''
+    meme = 'standard'
 
-	args_len = len(sys.argv)
-	topString = ''
-	meme = 'standard'
+    if args_len == 1:
+        # no args except the launch of the script
+        print('at least one argument is required')
 
-	if args_len == 1:
-		# no args except the launch of the script
-		print('args plz')
+    elif args_len == 2:
+        # only one argument, use standard meme
+        bottom_string = get_upper(sys.argv[-1])
 
-	elif args_len == 2:
-		# only one argument, use standard meme
-		bottomString = get_upper(sys.argv[-1])
+    elif args_len == 3:
+        # args give meme and one line
+        bottom_string = get_upper(sys.argv[-1])
+        meme = get_lower(sys.argv[1])
 
-	elif args_len == 3:
-		# args give meme and one line
-		bottomString = get_upper(sys.argv[-1])
-		meme = get_lower(sys.argv[1])
+    elif args_len == 4:
+        # args give meme and two lines
+        top_string = get_upper(sys.argv[-2])
+        bottom_string = get_upper(sys.argv[-1])
+        meme = get_lower(sys.argv[1])
+    else:
+        # so many args
+        # what do they mean
+        # too intense
+        print('too many arguments (max: 4)')
 
-	elif args_len == 4:
-		# args give meme and two lines
-		topString = get_upper(sys.argv[-2])
-		bottomString = get_upper(sys.argv[-1])
-		meme = get_lower(sys.argv[1])
-	else:
-		# so many args
-		# what do they mean
-		# too intense
-		print('to many argz')
-
-	print(meme)	
-	filename = str(meme)+'.jpg'
-	make_meme(topString, bottomString, filename)	
+    print(meme)	
+    filename = f'{meme}.jpg'
+    make_meme(top_string, bottom_string, filename)	
 
